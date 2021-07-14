8faf204c4fd3a7ce438a3177302ebddd7e1b0d63
Tech4All <tech4all583@gmail.com>
2021-07-14 09:18:21 +0100

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,21 @@
+<footer class="footer has-background-primary">
+    <div class="content has-text-centered has-text-white">
+        <p class="has-text-white has-text-centered">
+        <strong class="has-text-white">Collins crown real estate development and management</strong></a>
+        </p>
+    </div>
+    <div class="has-text-centered has-text-white">
+        <span class="icon">
+            <i class="fab fa-facebook-f"></i>
+        </span>
+        <span class="icon">
+            <i class="fab fa-twitter"></i>
+        </span>
+        <span class="icon">
+            <i class="fab fa-pinterest"></i>
+        </span>
+        <span class="icon">
+            <i class="fab fa-linkedin-in"></i>
+        </span>
+    </div>
+</footer>
